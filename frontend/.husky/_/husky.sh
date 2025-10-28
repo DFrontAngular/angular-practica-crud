27a50b75dcd3fe3f6003aaf16860065bf8215b25
Jacobo Enrique Ruiz Jimenez <jrjimene@emeal.nttdata.com>
2025-10-28 09:52:31 +0100

    test: probando husky manual

---
+++ frontend/.husky/_/husky.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+if [ -z "$husky_skip_init" ]; then
+  husky_skip_init=1
+  debug(){
+    ["$HUSKY_DEBUG" = "1" ] && echo "husky (debug) - $1"
+  }
+
+  readonly hook_name="$(basename "$0")"
+  debug "starting $hook_name hook"
+
+  readonly hook_dir="$(cd "$(dirname "$0")" && pwd -P)"
+  readonly husky_dir="$(dirname "$hook_dir")"
+  readonly husky_root="$(cd "$husky_dir/.." && pwd -P)"
+
+  if [ "$husky_dir" = "$husky_root/.husky" ]; then
+    export husky_root
+    export PATH="$husky_root/node_modules/.bin:$PATH"
+    . "$husky_dir/$hook_name"
+  fi
+fi
