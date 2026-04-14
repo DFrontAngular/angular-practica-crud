9a6d7ffa435da27d666a07bcb127279f5a597797
Pablo Morralla <pmorrall@emeal.nttdata.com>
2026-04-14 12:53:58 +0200

    Now from loads all brands with their models

--- frontend/src/app/pages/form/form.ts
+++ frontend/src/app/pages/form/form.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal, Signal } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { CarBrandDto } from '../../../model/DTO/car-brand-dto';
+import { CarModelDto } from '../../../model/DTO/car-model-dto';
+import { CarsService } from '../../../services/cars-service/cars-service';
+import { BrandDao } from '../../../model/DAO/brand-dao';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ModelDao } from '../../../model/DAO/model-dao';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-form',
@@ -6,4 +14,59 @@ import { Component } from '@angular/core';
   templateUrl: './form.html',
   styleUrl: './form.css',
 })
-export class Form {}
+export class Form {
+  private formBuilder = inject(FormBuilder);
+  private carsService = inject(CarsService);
+
+  error = signal<HttpErrorResponse|null>(null);
+  loading = signal(false);
+  brands = signal<BrandDao[]>([]);
+
+  constructor(){
+    this.loadBrands();
+  }
+
+  loadBrands(){
+    this.loading.set(true);
+    this.error.set(null);
+
+    this.carsService.getBrands().subscribe({
+      next: (brands) => {
+        const requests = brands.map( brand => 
+          this.carsService.getModelsByBrand(brand.id)
+        )
+
+        forkJoin(requests).subscribe({
+
+          next: (modelArrays) => {
+            this.brands.set( 
+              brands.map<BrandDao>((brand, i) => ({
+                id: brand.id,
+                name: brand.name,
+                models: modelArrays[i]
+              }))
+            )
+          },
+
+          error: (error) => {
+            this.loading.set(false);
+            this.error.set(error);
+            this.brands.set([]);
+          },
+
+          complete: () => {
+            this.loading.set(false);
+            this.error.set(null);
+          }
+          
+        })
+      },
+      
+      error: (error) => {
+        this.loading.set(false);
+        this.error.set(error);
+        this.brands.set([]);
+      }
+    });
+  }
+}
