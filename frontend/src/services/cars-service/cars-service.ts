e2f84e82879ba8937cbe3d394c3dcbdaa908ff61
Pablo Morralla <pmorrall@emeal.nttdata.com>
2026-04-14 10:50:00 +0200

    Added methods to CarsService to get brands and models

--- frontend/src/services/cars-service/cars-service.ts
+++ frontend/src/services/cars-service/cars-service.ts
@@ -3,6 +3,9 @@ import { HttpClient } from '@angular/common/http';
 import { PaginatedResponseDto } from '../../model/DTO/paginated-response-dto';
 import { CarDetailDto } from '../../model/DTO/car-detail-dto';
 import { CreateCarDto } from '../../model/DTO/create-car-dto';
+import { CarBrandDto } from '../../model/DTO/car-brand-dto';
+import { Observable } from 'rxjs';
+import { CarModelDto } from '../../model/DTO/car-model-dto';
 
 
 @Injectable({
@@ -10,33 +13,44 @@ import { CreateCarDto } from '../../model/DTO/create-car-dto';
 })
 export class CarsService {
   baseUrl: string = '/api';
+  timeout = 3000;
 
   constructor (private http: HttpClient) {}
 
-  public getCarList(page: number){
+  public getCarList(page: number): Observable<PaginatedResponseDto> {
     return this.http.get<PaginatedResponseDto>(
       `${this.baseUrl}/cars`,
       { 
         params: {
           page: page
         },
-        timeout: 3000 
+        timeout: this.timeout 
       }
     )
   }
 
-  public getCarDetails(carId: string) {
+  public getCarDetails(carId: string): Observable<CarDetailDto> {
     return this.http.get<CarDetailDto>(
       `${this.baseUrl}/cars/${carId}`,
       {
-        timeout: 3000
+        timeout: this.timeout
       }
     )
   }
 
-  public createCar(car: CreateCarDto) {
-    return this.http.post(
+  public createCar(car: CreateCarDto): Observable<CarDetailDto> {
+    return this.http.post<CarDetailDto>(
       `${this.baseUrl}/cars`, 
+      car,
+      {
+        timeout: this.timeout
+      }
+    )
+  }
+
+  public editCar(carId: number, car: CreateCarDto): Observable<CarDetailDto> {
+    return this.http.put<CarDetailDto>(
+      `${this.baseUrl}/cars/${carId}`,
       car,
       {
         timeout: 3000
@@ -44,10 +58,18 @@ export class CarsService {
     )
   }
 
-  public editCar(carId: number, car: CreateCarDto) {
-    return this.http.put(
-      `${this.baseUrl}/cars/${carId}`,
-      car,
+  public getBrands(): Observable<CarBrandDto[]> {
+    return this.http.get<CarBrandDto[]>(
+      `${this.baseUrl}/brands`,
+      {
+        timeout: 3000
+      }
+    )
+  }
+
+  public getModelsByBrand(brandId: string): Observable<CarModelDto[]> {
+    return this.http.get<CarModelDto[]>(
+      `${this.baseUrl}/brands/${brandId}/models`,
       {
         timeout: 3000
       }
